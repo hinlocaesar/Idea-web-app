95390cf5ed80a1c90064014e6c0d4cb1b57934e8
Caesar Herman N Hinlo <caesar.hinlo.tech@gmail.com>
2026-02-19 09:49:51 +0800

    Registration Form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,13 @@
 <x-layout>
-    <h1>Register</h1>
+    <fieldset class="fieldset bg-base-200 border-base-300 border-white rounded-box w-xs border p-4 mx-auto">
+    <legend class="fieldset-legend">Register</legend>
+
+    <label class="label">Email</label>
+    <input type="email" class="input" placeholder="Email" />
+
+    <label class="label">Password</label>
+    <input type="password" class="input" placeholder="Password" />
+
+    <button class="btn btn-neutral mt-4">Login</button>
+    </fieldset>
 </x-layout>
